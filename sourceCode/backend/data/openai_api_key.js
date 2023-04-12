b228f0599f71bc8e8fe529547d9753e5e09f8e67
ivanterzic <ivan.terzic@fer.hr>
2023-04-12 15:43:39 +0200

    changed api key

--- sourceCode/backend/data/openai_api_key.js
+++ sourceCode/backend/data/openai_api_key.js
@@ -1,1 +1,1 @@
-exports.OPENAI_API_KEY = "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"
+exports.OPENAI_API_KEY = "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"
--- sourceCode/frontend/src/App.js
+++ sourceCode/frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import {useEffect, useContext, useState} from "react";
 import './App.css';
-import SelectOption from './Components/SelectOption/SelectOption'
-import RadioContext from './Context/RadioContext';
-import PersonaContext from './Context/PersonaContext';
-import Chatbot from './Components/Chatbot/Chatbot';
-import Interactive from './Components/Interactive/Interactive'
-import axios from 'axios';
+
+
+/*"start": "PORT=4300 react-scripts start",*/
 
 function App() {
   return (
