ddfaf8543b1c1cec014a35287ac3ff56a3a00da5
ivanterzic <ivan.terzic@fer.hr>
2023-04-28 20:43:53 +0200

    Connected persona routes do database

--- sourceCode/backend/routes/persona.routes.js
+++ sourceCode/backend/routes/persona.routes.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const persona = require('../data/db')
+const db = require('../db')
 
 //returning a list of persona for the user to choose from
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    const sql = `SELECT personaId AS id, personaName AS name, personaGender AS gender, personaImageId AS imageid, personaInitialPrompt AS initialPrompt, personaVoice AS voice, personaCategoryId AS category, creatorUserName, originalPersona
+    FROM persona`;
     try {
-        res.json(categories);
+        const result = await db.query(sql, []);
+        console.log(result.rows);
+        res.json(result.rows);
     }
     catch (e) {
+        console.log(e)
         res.status(429)
         res.send()
     }
