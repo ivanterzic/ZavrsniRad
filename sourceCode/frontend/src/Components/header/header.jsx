bd124f1c6fc4015ec2362eca2b00d12350dd93c7
ivanterzic <ivan.terzic@fer.hr>
2023-05-14 10:45:39 +0200

    Update header.jsx

--- sourceCode/frontend/src/Components/header/header.jsx
+++ sourceCode/frontend/src/Components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './header.css'
+import './Header.css'
 import backend from '../../backendAPI';
 import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
